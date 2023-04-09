23342bca6510696edeef9070bee14b34b52fe2b6
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-09 19:20:43 +0530

    test

--- packit/index.js
+++ packit/index.js
@@ -1,16 +1,14 @@
 import "./.reejs/cache/1ad36c.js";
 	  import inline from "./.reejs/cache/538359.js";
 import tw from "./.reejs/serve/f42132.js";
-	  import fs from "node:fs";
+	  
 	  import ReeServer from "./node_modules/@reejs/server/index.js";
-			
+			import { serve } from 'https://deno.land/std/http/server.ts'
 		import { Hono } from "./.reejs/cache/c1dd1c.js";
-		import { serve } from "./.reejs/cache/cfa30f.js";
-	    import { serveStatic } from "./.reejs/cache/b98189.js"
 		import render from "./.reejs/cache/f086cf.js";
 		import React from "./.reejs/cache/08fbb2.js";
 		import App from "./.reejs/serve/fbc9a1.js";
-		const server = new ReeServer(Hono, {serve,});
+		const server = new ReeServer(Hono, {});
 	
 		import file_f629bb from "./.reejs/serve/f629bb.js";
 							server.app.get("/",(c)=>{ let h = "<!DOCTYPE html>"+render(React.createElement(App,null,React.createElement(file_f629bb,null))).replace('<script id="__reejs"></script>','<script type="importmap">{"imports":{"debug":"https://esm.sh/preact@10.13.2/debug","react":"https://esm.sh/preact@10.13.2/compat","@twind/core":"https://cdn.jsdelivr.net/npm/@twind/core/+esm","@twind/preset-autoprefix":"https://cdn.jsdelivr.net/npm/@twind/preset-autoprefix/+esm","@twind/preset-tailwind":"https://cdn.jsdelivr.net/npm/@twind/preset-tailwind/+esm","@twind/with-react":"https://cdn.jsdelivr.net/npm/@twind/with-react/+esm","@twind/with-react/inline":"https://cdn.jsdelivr.net/npm/@twind/with-react/inline/+esm","/__reejs/cache/08fbb2.js":"https://esm.sh/preact@10.13.2/compat","../cache/08fbb2.js":"https://esm.sh/preact@10.13.2/compat"}}</script><script type="module">let i=(await import("/__reejs/serve/4719e7.js")).default;i("f629bb.js",null);</script>');return c.html(inline(h,tw))});
@@ -21,70 +19,8 @@ import tw from "./.reejs/serve/f42132.js";
 import * as file_a0795a from "./.reejs/serve/a0795a.js";server.app[file_a0795a?.method?.toLowerCase() || "get"]("/api/bot/",file_a0795a.default);
 import * as file_eb6694 from "./.reejs/serve/eb6694.js";server.app[file_eb6694?.method?.toLowerCase() || "get"]("/api/bot/manage",file_eb6694.default);
 
-let cache_00acbcjs = fs.readFileSync("./.reejs/cache/00acbc.js");server.app.get("/__reejs/cache/00acbc.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_00acbcjs)});
-let cache_00bea4js = fs.readFileSync("./.reejs/cache/00bea4.js");server.app.get("/__reejs/cache/00bea4.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_00bea4js)});
-let cache_05e97ejs = fs.readFileSync("./.reejs/cache/05e97e.js");server.app.get("/__reejs/cache/05e97e.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_05e97ejs)});
-let cache_068ad4js = fs.readFileSync("./.reejs/cache/068ad4.js");server.app.get("/__reejs/cache/068ad4.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_068ad4js)});
-let cache_08fbb2js = fs.readFileSync("./.reejs/cache/08fbb2.js");server.app.get("/__reejs/cache/08fbb2.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_08fbb2js)});
-let cache_0b285djs = fs.readFileSync("./.reejs/cache/0b285d.js");server.app.get("/__reejs/cache/0b285d.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_0b285djs)});
-let cache_1ad36cjs = fs.readFileSync("./.reejs/cache/1ad36c.js");server.app.get("/__reejs/cache/1ad36c.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_1ad36cjs)});
-let cache_2ccf55js = fs.readFileSync("./.reejs/cache/2ccf55.js");server.app.get("/__reejs/cache/2ccf55.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_2ccf55js)});
-let cache_2f3538js = fs.readFileSync("./.reejs/cache/2f3538.js");server.app.get("/__reejs/cache/2f3538.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_2f3538js)});
-let cache_3b1bd0js = fs.readFileSync("./.reejs/cache/3b1bd0.js");server.app.get("/__reejs/cache/3b1bd0.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_3b1bd0js)});
-let cache_403ed2js = fs.readFileSync("./.reejs/cache/403ed2.js");server.app.get("/__reejs/cache/403ed2.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_403ed2js)});
-let cache_46ca48js = fs.readFileSync("./.reejs/cache/46ca48.js");server.app.get("/__reejs/cache/46ca48.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_46ca48js)});
-let cache_531a85js = fs.readFileSync("./.reejs/cache/531a85.js");server.app.get("/__reejs/cache/531a85.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_531a85js)});
-let cache_538359js = fs.readFileSync("./.reejs/cache/538359.js");server.app.get("/__reejs/cache/538359.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_538359js)});
-let cache_6f2c40js = fs.readFileSync("./.reejs/cache/6f2c40.js");server.app.get("/__reejs/cache/6f2c40.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_6f2c40js)});
-let cache_7c04cfjs = fs.readFileSync("./.reejs/cache/7c04cf.js");server.app.get("/__reejs/cache/7c04cf.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_7c04cfjs)});
-let cache_8afaa5js = fs.readFileSync("./.reejs/cache/8afaa5.js");server.app.get("/__reejs/cache/8afaa5.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_8afaa5js)});
-let cache_8c660cjs = fs.readFileSync("./.reejs/cache/8c660c.js");server.app.get("/__reejs/cache/8c660c.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_8c660cjs)});
-let cache_93c086js = fs.readFileSync("./.reejs/cache/93c086.js");server.app.get("/__reejs/cache/93c086.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_93c086js)});
-let cache_95956ejs = fs.readFileSync("./.reejs/cache/95956e.js");server.app.get("/__reejs/cache/95956e.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_95956ejs)});
-let cache_99bebcjs = fs.readFileSync("./.reejs/cache/99bebc.js");server.app.get("/__reejs/cache/99bebc.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_99bebcjs)});
-let cache_9bbad0js = fs.readFileSync("./.reejs/cache/9bbad0.js");server.app.get("/__reejs/cache/9bbad0.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_9bbad0js)});
-let cache_9c1c02js = fs.readFileSync("./.reejs/cache/9c1c02.js");server.app.get("/__reejs/cache/9c1c02.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_9c1c02js)});
-let cache_9c64bdjs = fs.readFileSync("./.reejs/cache/9c64bd.js");server.app.get("/__reejs/cache/9c64bd.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_9c64bdjs)});
-let cache_a0ed04js = fs.readFileSync("./.reejs/cache/a0ed04.js");server.app.get("/__reejs/cache/a0ed04.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_a0ed04js)});
-let cache_a19639js = fs.readFileSync("./.reejs/cache/a19639.js");server.app.get("/__reejs/cache/a19639.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_a19639js)});
-let cache_a8f378js = fs.readFileSync("./.reejs/cache/a8f378.js");server.app.get("/__reejs/cache/a8f378.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_a8f378js)});
-let cache_aa7314js = fs.readFileSync("./.reejs/cache/aa7314.js");server.app.get("/__reejs/cache/aa7314.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_aa7314js)});
-let cache_aae429js = fs.readFileSync("./.reejs/cache/aae429.js");server.app.get("/__reejs/cache/aae429.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_aae429js)});
-let cache_ae2adajs = fs.readFileSync("./.reejs/cache/ae2ada.js");server.app.get("/__reejs/cache/ae2ada.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_ae2adajs)});
-let cache_afc276js = fs.readFileSync("./.reejs/cache/afc276.js");server.app.get("/__reejs/cache/afc276.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_afc276js)});
-let cache_b3e22cjs = fs.readFileSync("./.reejs/cache/b3e22c.js");server.app.get("/__reejs/cache/b3e22c.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_b3e22cjs)});
-let cache_b98189js = fs.readFileSync("./.reejs/cache/b98189.js");server.app.get("/__reejs/cache/b98189.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_b98189js)});
-let cache_b9d525js = fs.readFileSync("./.reejs/cache/b9d525.js");server.app.get("/__reejs/cache/b9d525.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_b9d525js)});
-let cache_c1dd1cjs = fs.readFileSync("./.reejs/cache/c1dd1c.js");server.app.get("/__reejs/cache/c1dd1c.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_c1dd1cjs)});
-let cache_c4d3b6js = fs.readFileSync("./.reejs/cache/c4d3b6.js");server.app.get("/__reejs/cache/c4d3b6.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_c4d3b6js)});
-let cache_c5d58bjs = fs.readFileSync("./.reejs/cache/c5d58b.js");server.app.get("/__reejs/cache/c5d58b.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_c5d58bjs)});
-let cache_cachejson = fs.readFileSync("./.reejs/cache/cache.json");server.app.get("/__reejs/cache/cache.json", (c)=>{c.header('Content-type','application/json');return c.body(cache_cachejson)});
-let cache_cfa30fjs = fs.readFileSync("./.reejs/cache/cfa30f.js");server.app.get("/__reejs/cache/cfa30f.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_cfa30fjs)});
-let cache_d18365js = fs.readFileSync("./.reejs/cache/d18365.js");server.app.get("/__reejs/cache/d18365.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_d18365js)});
-let cache_e1b0bcjs = fs.readFileSync("./.reejs/cache/e1b0bc.js");server.app.get("/__reejs/cache/e1b0bc.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_e1b0bcjs)});
-let cache_e28040js = fs.readFileSync("./.reejs/cache/e28040.js");server.app.get("/__reejs/cache/e28040.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_e28040js)});
-let cache_e4548ejs = fs.readFileSync("./.reejs/cache/e4548e.js");server.app.get("/__reejs/cache/e4548e.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_e4548ejs)});
-let cache_e7e546js = fs.readFileSync("./.reejs/cache/e7e546.js");server.app.get("/__reejs/cache/e7e546.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_e7e546js)});
-let cache_eb666djs = fs.readFileSync("./.reejs/cache/eb666d.js");server.app.get("/__reejs/cache/eb666d.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_eb666djs)});
-let cache_f086cfjs = fs.readFileSync("./.reejs/cache/f086cf.js");server.app.get("/__reejs/cache/f086cf.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_f086cfjs)});
-let cache_f457b7js = fs.readFileSync("./.reejs/cache/f457b7.js");server.app.get("/__reejs/cache/f457b7.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_f457b7js)});
-let cache_f788fbjs = fs.readFileSync("./.reejs/cache/f788fb.js");server.app.get("/__reejs/cache/f788fb.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_f788fbjs)});
-let cache_f7fdbfjs = fs.readFileSync("./.reejs/cache/f7fdbf.js");server.app.get("/__reejs/cache/f7fdbf.js", (c)=>{c.header('Content-type','text/javascript');return c.body(cache_f7fdbfjs)});
-let cache_packagejson = fs.readFileSync("./.reejs/cache/package.json");server.app.get("/__reejs/cache/package.json", (c)=>{c.header('Content-type','application/json');return c.body(cache_packagejson)});let serve_3d460fjs = fs.readFileSync("./.reejs/serve/3d460f.js");server.app.get("/__reejs/serve/3d460f.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_3d460fjs)});
-let serve_4719e7js = fs.readFileSync("./.reejs/serve/4719e7.js");server.app.get("/__reejs/serve/4719e7.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_4719e7js)});
-let serve_4ef0cbjs = fs.readFileSync("./.reejs/serve/4ef0cb.js");server.app.get("/__reejs/serve/4ef0cb.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_4ef0cbjs)});
-let serve_5052c2js = fs.readFileSync("./.reejs/serve/5052c2.js");server.app.get("/__reejs/serve/5052c2.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_5052c2js)});
-let serve_5ec234js = fs.readFileSync("./.reejs/serve/5ec234.js");server.app.get("/__reejs/serve/5ec234.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_5ec234js)});
-let serve_7fbb77js = fs.readFileSync("./.reejs/serve/7fbb77.js");server.app.get("/__reejs/serve/7fbb77.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_7fbb77js)});
-let serve_8767e5js = fs.readFileSync("./.reejs/serve/8767e5.js");server.app.get("/__reejs/serve/8767e5.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_8767e5js)});
-let serve_a0795ajs = fs.readFileSync("./.reejs/serve/a0795a.js");server.app.get("/__reejs/serve/a0795a.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_a0795ajs)});
-let serve_a98e48js = fs.readFileSync("./.reejs/serve/a98e48.js");server.app.get("/__reejs/serve/a98e48.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_a98e48js)});
-let serve_b1786cjs = fs.readFileSync("./.reejs/serve/b1786c.js");server.app.get("/__reejs/serve/b1786c.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_b1786cjs)});
-let serve_eb6694js = fs.readFileSync("./.reejs/serve/eb6694.js");server.app.get("/__reejs/serve/eb6694.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_eb6694js)});
-let serve_f42132js = fs.readFileSync("./.reejs/serve/f42132.js");server.app.get("/__reejs/serve/f42132.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_f42132js)});
-let serve_f629bbjs = fs.readFileSync("./.reejs/serve/f629bb.js");server.app.get("/__reejs/serve/f629bb.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_f629bbjs)});
-let serve_fbc9a1js = fs.readFileSync("./.reejs/serve/fbc9a1.js");server.app.get("/__reejs/serve/fbc9a1.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_fbc9a1js)});
-		server.app.get('/**',serveStatic({root:'./public'}));server.listen(process.env.PORT || 3000, () => console.log(`Server started on port ${process.env.PORT || 3000}`));
+
+		
 	
-	
+	serve(server.app.fetch)
 			
