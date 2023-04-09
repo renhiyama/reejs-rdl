3be6e997e3edadf7b13ea5ae87a2b2264db77d16
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-09 17:50:35 +0000

    test

--- packit/.reejs/serve/eb6694.js
+++ packit/.reejs/serve/eb6694.js
@@ -1,1 +1,1 @@
-import r from"../..//src/bot/register.js";export default function e(e){e.req.query("register")&&r()}
+import r from"../../src/bot/register.js";export default function e(e){e.req.query("register")&&r()}
