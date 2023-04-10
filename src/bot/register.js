a910698b11f6076b05f032ff807c14d9cebff29d
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-10 10:36:10 +0000

    test

--- src/bot/register.js
+++ src/bot/register.js
@@ -1,8 +1,9 @@
 import eureka, {run as eureka_run} from "./commands/eureka.js";
+import info, {run as info_run} from "./commands/info.js";
 import ping, {run as ping_run} from "./commands/ping.js";
 
-export const commands = [ ping, eureka ];
-export const runs = [ ping_run, eureka_run ];
+export const commands = [ ping, eureka, info ];
+export const runs = [ ping_run, eureka_run, info_run ];
 export default async function registerCommands() {
   for (const command of commands) {
     console.log(`%c[DISCORD] %cRegistering command %c${command.name}`,
