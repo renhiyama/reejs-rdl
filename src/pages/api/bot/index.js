e4d4507c224088efa5a31c76622071bb5d6bb78b
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-10 01:03:51 +0000

    test

--- src/pages/api/bot/index.js
+++ src/pages/api/bot/index.js
@@ -2,7 +2,7 @@ import {
   InteractionResponseType,
   InteractionType,
 } from 'discord-interactions';
-import {Buffer} from "https://deno.land/std@0.182.0/io/buffer.ts";
+import {Buffer} from "https://deno.land/std@0.110.0/node/buffer.ts";
 import nacl from "tweetnacl";
 
 import {commands, runs} from '../../../bot/register.js';
