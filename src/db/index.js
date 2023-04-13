1c65e11afb6a5453ce72cbae25c2bf611ff2d7c2
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-13 07:35:01 +0000

    test

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,9 @@ import Bots from "./models/bots";
 import Servers from "./models/servers";
 import Users from "./models/users";
 
+console.log("Connecting to MongoDB...");
 await mongoose.connect(Deno.env.get("DB_URL"));
-console.log("%c[DB] %cConnected to Database", "color: #eb4590", "");
+console.log("%c[DB] Connected to Database: " + mongoose.connection.readyState,
+            "color: #eb4590");
 
 export default {Bots, Servers, Users};
