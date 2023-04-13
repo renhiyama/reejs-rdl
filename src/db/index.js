4e6a2c028ba219be56ebc559af2963817f6f4171
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-13 07:59:03 +0000

    test

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import Bots from "./models/bots.js";
 import Servers from "./models/servers.js";
 import Users from "./models/users.js";
 
-console.log(mongoose);
+console.log("uwu", mongoose);
 await mongoose.connect(Deno.env.get("DB_URL"));
 console.log("%c[DB] Connected to Database: " + mongoose.connection.readyState,
             "color: #eb4590");
