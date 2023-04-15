213154fff4b86322434b26e5c710e300eb466bbc
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-15 16:54:02 +0530

    hmm

--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,10 @@ console.log(Deno.env.get("DB_URL"), Deno.env.get("DB_USER"), Deno.env.get("DB_PA
 
 const db = new Surreal(Deno.env.get("DB_URL"));
 
-let user = Deno.env.get("DB_USER");
-let password = Deno.env.get("DB_PASSWORD");
-
-await db.signin({ user, password });
+await db.signin({
+  user: Deno.env.get("DB_USER"),
+  pass: Deno.env.get("DB_PASSWORD"),
+});
 
 console.log("Connected to database");
 
