0f09dc5478b699ec34739079cbf45e8a28485c72
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-16 09:15:59 +0530

    stuff

--- packit/index.js
+++ packit/index.js
@@ -1,42 +1,26 @@
 import "https://esm.sh/preact@10.13.2/debug";
 	  import inline from "https://cdn.jsdelivr.net/npm/@twind/with-react/inline/+esm";
-import tw from "./.reejs/serve/b9124b.js";
+import tw from "./.reejs/serve/f30b47.js";
 	  
 	  import ReeServer from "./node_modules/@reejs/server/index.js";
 			import { serve } from 'https://deno.land/std/http/server.ts'
 		import { Hono } from "https://esm.sh/hono@3.0.3?bundle";
 		import render from "https://esm.sh/preact-render-to-string@6.0.2";
 		import React from "https://esm.sh/preact@10.13.2/compat";
-		import App from "./.reejs/serve/693ad1.js";
+		import App from "./.reejs/serve/ecc2a5.js";
 		const server = new ReeServer(Hono, {});
 	
-		import file_6be6cd from "./.reejs/serve/6be6cd.js";
-							server.app.get("/",(c)=>{ let h = "<!DOCTYPE html>"+render(React.createElement(App,null,React.createElement(file_6be6cd,null))).replace('<script id="__reejs"></script>','<script type="importmap">{"imports":{"debug":"https://esm.sh/preact@10.13.2/debug","react":"https://esm.sh/preact@10.13.2/compat","@twind/core":"https://cdn.jsdelivr.net/npm/@twind/core/+esm","@twind/preset-autoprefix":"https://cdn.jsdelivr.net/npm/@twind/preset-autoprefix/+esm","@twind/preset-tailwind":"https://cdn.jsdelivr.net/npm/@twind/preset-tailwind/+esm","@twind/with-react":"https://cdn.jsdelivr.net/npm/@twind/with-react/+esm","@twind/with-react/inline":"https://cdn.jsdelivr.net/npm/@twind/with-react/inline/+esm"}}</script><script type="module">let i=(await import("/__reejs/serve/790f31.js")).default;i("6be6cd.js",null);</script>');return c.html(inline(h,tw))});
+		import file_50533a from "./.reejs/serve/50533a.js";
+							server.app.get("/",(c)=>{ let h = "<!DOCTYPE html>"+render(React.createElement(App,null,React.createElement(file_50533a,null))).replace('<script id="__reejs"></script>','<script type="importmap">{"imports":{"debug":"https://esm.sh/preact@10.13.2/debug","react":"https://esm.sh/preact@10.13.2/compat","@twind/core":"https://cdn.jsdelivr.net/npm/@twind/core/+esm","@twind/preset-autoprefix":"https://cdn.jsdelivr.net/npm/@twind/preset-autoprefix/+esm","@twind/preset-tailwind":"https://cdn.jsdelivr.net/npm/@twind/preset-tailwind/+esm","@twind/with-react":"https://cdn.jsdelivr.net/npm/@twind/with-react/+esm","@twind/with-react/inline":"https://cdn.jsdelivr.net/npm/@twind/with-react/inline/+esm"}}</script><script type="module">let i=(await import("/__reejs/serve/ebc480.js")).default;i("50533a.js",null);</script>');return c.html(inline(h,tw))});
 							
 			
 		
-		import * as file_a31cf9 from "./.reejs/serve/a31cf9.js";server.app[file_a31cf9?.method?.toLowerCase() || "get"]("/api/bot/",file_a31cf9.default);
-import * as file_3348ba from "./.reejs/serve/3348ba.js";server.app[file_3348ba?.method?.toLowerCase() || "get"]("/api/bot/manage",file_3348ba.default);
-import * as file_b867de from "./.reejs/serve/b867de.js";server.app[file_b867de?.method?.toLowerCase() || "get"]("/api/bots/:id",file_b867de.default);
-import * as file_166649 from "./.reejs/serve/166649.js";server.app[file_166649?.method?.toLowerCase() || "get"]("/api/bots/new",file_166649.default);
+		import * as file_1d1f96 from "./.reejs/serve/1d1f96.js";server.app[file_1d1f96?.method?.toLowerCase() || "get"]("/api/bot/",file_1d1f96.default);
+import * as file_e9d733 from "./.reejs/serve/e9d733.js";server.app[file_e9d733?.method?.toLowerCase() || "get"]("/api/bot/manage",file_e9d733.default);
+import * as file_9a7f67 from "./.reejs/serve/9a7f67.js";server.app[file_9a7f67?.method?.toLowerCase() || "get"]("/api/bots/:id",file_9a7f67.default);
+import * as file_f5140a from "./.reejs/serve/f5140a.js";server.app[file_f5140a?.method?.toLowerCase() || "get"]("/api/bots/new",file_f5140a.default);
 
-		let serve_00dbcdjs = await Deno.readFile("./.reejs/serve/00dbcd.js");server.app.get("/__reejs/serve/00dbcd.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_00dbcdjs)});
-let serve_166649js = await Deno.readFile("./.reejs/serve/166649.js");server.app.get("/__reejs/serve/166649.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_166649js)});
-let serve_3348bajs = await Deno.readFile("./.reejs/serve/3348ba.js");server.app.get("/__reejs/serve/3348ba.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_3348bajs)});
-let serve_387a65js = await Deno.readFile("./.reejs/serve/387a65.js");server.app.get("/__reejs/serve/387a65.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_387a65js)});
-let serve_46c8ecjs = await Deno.readFile("./.reejs/serve/46c8ec.js");server.app.get("/__reejs/serve/46c8ec.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_46c8ecjs)});
-let serve_68fe99js = await Deno.readFile("./.reejs/serve/68fe99.js");server.app.get("/__reejs/serve/68fe99.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_68fe99js)});
-let serve_693ad1js = await Deno.readFile("./.reejs/serve/693ad1.js");server.app.get("/__reejs/serve/693ad1.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_693ad1js)});
-let serve_6be6cdjs = await Deno.readFile("./.reejs/serve/6be6cd.js");server.app.get("/__reejs/serve/6be6cd.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_6be6cdjs)});
-let serve_712ab2js = await Deno.readFile("./.reejs/serve/712ab2.js");server.app.get("/__reejs/serve/712ab2.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_712ab2js)});
-let serve_790f31js = await Deno.readFile("./.reejs/serve/790f31.js");server.app.get("/__reejs/serve/790f31.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_790f31js)});
-let serve_a31cf9js = await Deno.readFile("./.reejs/serve/a31cf9.js");server.app.get("/__reejs/serve/a31cf9.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_a31cf9js)});
-let serve_b867dejs = await Deno.readFile("./.reejs/serve/b867de.js");server.app.get("/__reejs/serve/b867de.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_b867dejs)});
-let serve_b9124bjs = await Deno.readFile("./.reejs/serve/b9124b.js");server.app.get("/__reejs/serve/b9124b.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_b9124bjs)});
-let serve_c2d913js = await Deno.readFile("./.reejs/serve/c2d913.js");server.app.get("/__reejs/serve/c2d913.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_c2d913js)});
-let serve_dd79e4js = await Deno.readFile("./.reejs/serve/dd79e4.js");server.app.get("/__reejs/serve/dd79e4.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_dd79e4js)});
-let serve_f58f25js = await Deno.readFile("./.reejs/serve/f58f25.js");server.app.get("/__reejs/serve/f58f25.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_f58f25js)});
-let serve_fadc48js = await Deno.readFile("./.reejs/serve/fadc48.js");server.app.get("/__reejs/serve/fadc48.js", (c)=>{c.header('Content-type','text/javascript');return c.body(serve_fadc48js)});
+		
 	  
 	
 	serve(server.app.fetch)
--- src/bot/commands/info.js
+++ src/bot/commands/info.js
@@ -3,120 +3,173 @@ export default {
   name: "info",
   description: "Get info from RDL",
   options:
-    [
-      {
-        name: "bot",
-        description: "Get info about bot",
-        type: 1,
-        options: [
-          {
-            name: "id",
-            description: "The bot's ID",
-            type: 6,
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "user",
-        description: "Get info about user",
-        type: 1,
-        options: [
-          {
-            name: "id",
-            description: "The user's ID (default: you)",
-            type: 6,
-          },
-        ],
-      }
-    ]
+      [
+        {
+          name : "bot",
+          description : "Get info about bot",
+          type : 1,
+          options : [
+            {
+              name : "id",
+              description : "The bot's ID",
+              type : 6,
+              required : true,
+            },
+          ],
+        },
+        {
+          name : "user",
+          description : "Get info about user",
+          type : 1,
+          options : [
+            {
+              name : "id",
+              description : "The user's ID (default: you)",
+              type : 6,
+            },
+          ],
+        }
+      ]
 };
 export async function run(c, msg) {
   let type = msg.data.options[0].name;
   let id = Object.keys(msg.data.resolved.members)[0] || msg.member.user.id;
   if (type == "bot") {
-    let bots = await db.query(`SELECT * FROM bots WHERE botID = $id`, { id });
+    let bots = await db.query(`SELECT * FROM bots WHERE botID = $id`, {id});
     let bot = bots[0].result[0];
+    let res =
+        await fetch(`https://discord.com/api/v10/interactions/${msg.id}/${
+                        msg.token}/callback`,
+                    {
+                      method : "POST",
+                      headers : {
+                        "Content-Type" : "application/json",
+                        "Authorization" : `Bot ${Deno.env.get("DISCORD_TOKEN")}`
+                      },
+                      body : JSON.stringify({
+                        type : 4,
+                        data : {
+                          content : "Searching...",
+                        }
+                      })
+                    });
+
     if (!bot) {
-      return c.json({ type: 4, data: { content: "Bot not found" } });
+      return await fetch(
+          `https://discord.com/api/v10/webhooks/${
+              Deno.env.get(
+                  "DISCORD_CLIENT_ID")}/${msg.token}/messages/@original`,
+          {
+            method : "PATCH",
+            headers : {
+              "Content-Type" : "application/json",
+              "Authorization" : `Bot ${Deno.env.get("DISCORD_TOKEN")}`
+            },
+            body : JSON.stringify({
+              type : 4,
+              data : {
+                content : "Bot not found",
+              }
+            })
+          });
     }
-    return c.json({
-      type: 4,
-      data: {
-        embeds: [{
-          title: bot.tag,
-          color: 0x5865f2,
-          url: `https://dscrdly.com/b/${bot.slug}`,
-          thumbnail: { url: bot.avatarURL },
-          fields: [
-            { name: "Prefix", value: bot.prefix || "<Not Disclosed>", inline: true },
-            { name: "Servers", value: bot.servers || "<Not Disclosed>", inline: true },
-            {
-              name: "Owner", value: bot.owners.map(owner => `<@!${owner}>`).join(", "), inline: true
-            }, {
-              name: "Library",
-              value: bot.lib || "<Not Disclosed>", inline: true
-            },
-            {
-              name: "Invite",
-              value: `[Click Here for No-Perm Invite](https://discord.com/oauth2/authorize?client_id=${bot.id}&scope=bot&permissions=0)\n[Click Here for Original Invite](${bot.invite})`, inline: true
-            },
-            {
-              name: "Support Server",
-              value: bot.support?`https://discord.gg/${bot.support}` : "<Not Disclosed>", inline: true
-            },
-            {
-              name: "Vote",
-              value: `[Click Here to Vote - ${bot.opted_coins
-                  ? "Uses R$"
-                  : "Based on Cooldown"}](https://discord.rovelstars.com/bots/${bot.id}/vote)`, inline: true
+    return await fetch(
+        `https://discord.com/api/v10/webhooks/${
+            Deno.env.get("DISCORD_CLIENT_ID")}/${msg.token}/messages/@original`,
+        {
+          method : "PATCH",
+          headers : {
+            "Content-Type" : "application/json",
+            "Authorization" : `Bot ${Deno.env.get("DISCORD_TOKEN")}`
+          },
+          body : JSON.stringify({
+            type : 4,
+            data : {
+              embeds : [ {
+                title : bot.tag,
+                color : 0x5865f2,
+                url : `https://dscrdly.com/b/${bot.slug}`,
+                thumbnail : {url : bot.avatarURL},
+                fields : [
+                  {
+                    name : "Prefix",
+                    value : bot.prefix || "<Not Disclosed>",
+                    inline : true
+                  },
+                  {
+                    name : "Servers",
+                    value : bot.servers || "<Not Disclosed>",
+                    inline : true
+                  },
+                  {
+                    name : "Owner",
+                    value : bot.owners.map(owner => `<@!${owner}>`).join(", "),
+                    inline : true
+                  },
+                  {
+                    name : "Library",
+                    value : bot.lib || "<Not Disclosed>",
+                    inline : true
+                  },
+                  {
+                    name : "Invite",
+                    value : `[Click Here for No-Perm Invite](https://discord.com/oauth2/authorize?client_id=${
+                        bot.id}&scope=bot&permissions=0)\n[Click Here for Original Invite](${
+                        bot.invite})`,
+                    inline : true
+                  },
+                  {
+                    name : "Support Server",
+                    value : bot.support ? `https://discord.gg/${bot.support}`
+                                        : "<Not Disclosed>",
+                    inline : true
+                  },
+                  {
+                    name : "Vote",
+                    value : `[Click Here to Vote - ${
+                        bot.opted_coins
+                            ? "Uses R$"
+                            : "Based on Cooldown"}](https://discord.rovelstars.com/bots/${
+                        bot.id}/vote)`,
+                    inline : true
+                  }
+                ]
+              } ]
             }
-          ]
-        }]
-      }
-    });
-    // let res = await fetch(`https://discord.com/api/v10/interactions/${msg.id}/${msg.token}/callback`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Authorization: `Bot ${Deno.env.get("TOKEN")}`
-    //   },
-    //   body: JSON.stringify(data)
-    // });
-    // res = await res.json();
-    // console.log(res);
+          })
+        });
 
   } else if (type == "user") {
     console.log(Users);
-    let user = await Users.findOne({ id });
-    let ownedBots = await Bots.find({ owners: { $in: [id] } });
+    let user = await Users.findOne({id});
+    let ownedBots = await Bots.find({owners : {$in : [ id ]}});
     let ownedBotsString =
-      ownedBots.length ? ownedBots.map(bot => bot.tag).join(", ") : "No Bots";
+        ownedBots.length ? ownedBots.map(bot => bot.tag).join(", ") : "No Bots";
     console.log(user);
     console.log(ownedBots);
     if (!user) {
       return c.json(
-        { type: 4, data: { content: "User not found", flags: 64 } });
+          {type : 4, data : {content : "User not found", flags : 64}});
     }
     return c.json({
-      type: 4,
-      data: {
-        embeds: [{
-          title: user.tag,
-          description: user.bio,
-          color: 0x5865f2,
-          thumbnail: { url: user.avatarURL },
-          fields: [
-            { name: "Bots", value: ownedBotsString, inline: true },
-            { name: "Balance", value: `R$ ${user.coins}`, inline: true }, {
-              name: "Recently Voted Bots",
-              value: user.votes.length
-                ? user.votes.map(bot => `<@!${bot}>`).join(", ")
-                : "No Bots Voted Recently",
-              inline: true
+      type : 4,
+      data : {
+        embeds : [ {
+          title : user.tag,
+          description : user.bio,
+          color : 0x5865f2,
+          thumbnail : {url : user.avatarURL},
+          fields : [
+            {name : "Bots", value : ownedBotsString, inline : true},
+            {name : "Balance", value : `R$ ${user.coins}`, inline : true}, {
+              name : "Recently Voted Bots",
+              value : user.votes.length
+                          ? user.votes.map(bot => `<@!${bot}>`).join(", ")
+                          : "No Bots Voted Recently",
+              inline : true
             }
           ]
-        }]
+        } ]
       }
     });
   }
